67a21b2be1092ce95612e5047f9bf8a1c2f84972
Sarah Jahan <sarah-jahan@live.com>
2024-11-13 14:22:08 -0500

    add mobile styling

--- src/components/WarehouseForm/WarehouseForm.jsx
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -1,16 +1,105 @@
 import "./WarehouseForm.scss";
 
-function WarehouseForm() {
+function WarehouseForm({action}) {
     return (
-      <div className="container">
-
+      <section className="container">
         <h2>Warehouse Details</h2>
-        <h2>Contact Details</h2>
+        <fieldset className="form__field">
+          <label className="form__label" htmlFor="warehouse_name">
+            Warehouse Name
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="warehouse_name"
+            name="warehouse_name"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="address">
+            Street Address
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="address"
+            name="address"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="city">
+            City
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="city"
+            name="city"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="country">
+            Country
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="country"
+            name="country"
+            placeholder="insert warehouse details prop"
+          />
+          
+        </fieldset>
 
-        
-        
+        <h2>Contact Details</h2>
+        <fieldset className="form__field">
+          <label className="form__label" htmlFor="contact_name">
+            Contact Name
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="contact_name"
+            name="contact_name"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="contact_position">
+            Position
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="contact_position"
+            name="contact_position"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="contact_phone">
+            Phone Number
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="contact_phone"
+            name="contact_phone"
+            placeholder="insert warehouse details prop"
+          />
+          <label className="form__label" htmlFor="contact_email">
+            Email
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="contact_email"
+            name="contact_email"
+            placeholder="insert warehouse details prop"
+          />
+        </fieldset>
+
+        <div className="submit-container">
+            <button className="button button-secondary">Cancel</button>
+            <button className="button button-primary">{action}</button>
+        </div>
+
+        </section>
+
   
-      </div>
     );
   }
   
