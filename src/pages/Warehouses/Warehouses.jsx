e0780b82a176993ba71861c36682b9ab2e3e9a18
Humaira Tasnim <hxtasnim@gmail.com>
2024-11-13 16:24:23 -0500

    add tablet and desktop styles for Warehouses listings

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -8,7 +8,6 @@ function Warehouses() {
   return (
     <main className="container">
       <section className="panel">
-
         <div className="panel__header">
           <h1 className="panel__title">Warehouses</h1>
           <input
@@ -24,54 +23,46 @@ function Warehouses() {
         </div>
 
         <div className="panel__body">
-
           <div className="table__row">
-
-            <div className="table__info">
-              <div className="table__column">
-                <div className="table__content-group">
-                  <h2 className="table-header">Warehouse</h2>
-                  <NavLink to="" className="link table-body">
-                    <span className="link__text">Manhattan</span>
-                    <img
-                      src={chevronIcon}
-                      alt="Chevron icon"
-                      className="link__icon"
-                    />
-                  </NavLink>
-                </div>
-                <div className="table__content-group">
-                  <h2 className="table-header">Address</h2>
-                  <div className="table-body">503 Broadway, New York, USA</div>
-                </div>
+            <div className="table__column">
+              <div className="table__content-group">
+                <h2 className="table__header">Warehouse</h2>
+                <NavLink to="" className="link table__body">
+                  <span className="link__text">Manhattan</span>
+                  <img
+                    src={chevronIcon}
+                    alt="Chevron icon"
+                    className="link__icon"
+                  />
+                </NavLink>
               </div>
-              <div className="table__column">
-                <div className="table__content-group">
-                  <h2 className="table-header">Contact Name</h2>
-                  <div className="table-body">Parmin Aujla</div>
-                </div>
-                <div className="table__content-group">
-                  <h2 className="table-header">Contact Information</h2>
-                  <div className="table-body">
-                    +1 (629) 555-0129 paujla@instock.com
-                  </div>
+              <div className="table__content-group">
+                <h2 className="table__header">Address</h2>
+                <div className="table__body">503 Broadway, New York, USA</div>
+              </div>
+            </div>
+            <div className="table__column">
+              <div className="table__content-group">
+                <h2 className="table__header">Contact Name</h2>
+                <div className="table__body">Parmin Aujla</div>
+              </div>
+              <div className="table__content-group">
+                <h2 className="table__header">Contact Information</h2>
+                <div className="table__body">
+                  +1 (629) 555-0129<br/>paujla@instock.com
                 </div>
               </div>
             </div>
-
             <div className="table__actions">
-              <button className="table__actions--delete-btn">
-                <img src={deleteIcon} className="icon" />
-              </button>
-              <button className="table__actions--edit-btn">
-                <img src={editIcon} className="icon" />
-              </button>
+                <button className="table__delete-btn">
+                  <img src={deleteIcon} className="icon" />
+                </button>
+                <button className="table__edit-btn">
+                  <img src={editIcon} className="icon" />
+                </button>
             </div>
-
           </div>
-
         </div>
-
       </section>
     </main>
   );
