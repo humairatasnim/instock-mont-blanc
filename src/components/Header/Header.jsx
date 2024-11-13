85c1790329a801b24c41a901ccbe3ed3a3bd35f8
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-13 10:56:10 -0500

    Creating header component with corresponding routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import "./Header.scss";
+import logo from "/src/assets/logo/InStock-Logo_1x.png";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-
   return (
-    <header>Header</header>
-  )
+    <>
+    <header>
+      <nav className="nav">
+        <div className="nav__logo-container"><NavLink to='/'><img src={logo} alt="websise logo" className="nav__logo" /></NavLink></div>
+        <ul className="nav__list">
+        <li className="nav__links-item"><NavLink to ='/inventory'>Inventory</NavLink></li>
+        <li className="nav__links-item"><NavLink to ='/'>Warehouses</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  );
 }
 
 export default Header;
