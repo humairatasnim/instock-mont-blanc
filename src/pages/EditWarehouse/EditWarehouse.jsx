b3b26094c3b3c6df1a5801fb98c3954d94795baa
Sarah Jahan <sarah-jahan@live.com>
2024-11-13 14:24:35 -0500

    add title container and form component

--- src/pages/EditWarehouse/EditWarehouse.jsx
+++ src/pages/EditWarehouse/EditWarehouse.jsx
@@ -1,10 +1,18 @@
+import WarehouseForm from "../../components/WarehouseForm/WarehouseForm";
 import "./EditWarehouse.scss";
+import backbutton from "../../assets/icons/arrow_back-24px.svg"
+
 
 function EditWarehouse() {
   return (
-    <>
-      <h1>Edit Warehouse</h1>
-    </>
+    <div className="container">
+      <section className="title-container">
+        <img className="icon" src={backbutton}/>
+        <h1 className="page-header">Edit Warehouse</h1>
+      </section>
+      
+      <WarehouseForm action={"Save"}/>
+    </div>
   );
 }
 
