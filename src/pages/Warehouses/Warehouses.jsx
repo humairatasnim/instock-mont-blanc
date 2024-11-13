73892beda929671612d8216fb820d6284e86c8ce
Humaira Tasnim <hxtasnim@gmail.com>
2024-11-13 17:30:43 -0500

    add header row for desktop and tablet

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import chevronIcon from "../../assets/icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
+import sortIcon from "../../assets/icons/sort-24px.svg";
 import "./Warehouses.scss";
 
 function Warehouses() {
@@ -23,10 +24,60 @@ function Warehouses() {
         </div>
 
         <div className="panel__body">
+          <div className="table__row table__header-row">
+            <div className="table__column">
+              <div className="table__content-group table__header-group">
+                <h2 className="table__header">Warehouse</h2>
+                <button className="table__sort-btn">
+                  <img
+                    src={sortIcon}
+                    alt="Sort icon"
+                    className="table__sort-icon"
+                  />
+                </button>
+              </div>
+              <div className="table__content-group table__header-group table__address">
+                <h2 className="table__header">Address</h2>
+                <button className="table__sort-btn">
+                  <img
+                    src={sortIcon}
+                    alt="Sort icon"
+                    className="table__sort-icon"
+                  />
+                </button>{" "}
+              </div>
+            </div>
+            <div className="table__column">
+              <div className="table__content-group table__header-group">
+                <h2 className="table__header">Contact Name</h2>
+                <button className="table__sort-btn">
+                  <img
+                    src={sortIcon}
+                    alt="Sort icon"
+                    className="table__sort-icon"
+                  />
+                </button>{" "}
+              </div>
+              <div className="table__content-group table__header-group table__contact-info">
+                <h2 className="table__header">Contact Information</h2>
+                <button className="table__sort-btn">
+                  <img
+                    src={sortIcon}
+                    alt="Sort icon"
+                    className="table__sort-icon"
+                  />
+                </button>{" "}
+              </div>
+            </div>
+            <div className="table__actions">
+              <h2 className="table__header">Actions</h2>
+            </div>
+          </div>
+
           <div className="table__row">
             <div className="table__column">
               <div className="table__content-group">
-                <h2 className="table__header">Warehouse</h2>
+                <h2 className="table__header--mobile">Warehouse</h2>
                 <NavLink to="" className="link table__body">
                   <span className="link__text">Manhattan</span>
                   <img
@@ -36,30 +87,32 @@ function Warehouses() {
                   />
                 </NavLink>
               </div>
-              <div className="table__content-group">
-                <h2 className="table__header">Address</h2>
+              <div className="table__content-group table__address">
+                <h2 className="table__header--mobile">Address</h2>
                 <div className="table__body">503 Broadway, New York, USA</div>
               </div>
             </div>
             <div className="table__column">
               <div className="table__content-group">
-                <h2 className="table__header">Contact Name</h2>
+                <h2 className="table__header--mobile">Contact Name</h2>
                 <div className="table__body">Parmin Aujla</div>
               </div>
-              <div className="table__content-group">
-                <h2 className="table__header">Contact Information</h2>
+              <div className="table__content-group table__contact-info">
+                <h2 className="table__header--mobile">Contact Information</h2>
                 <div className="table__body">
-                  +1 (629) 555-0129<br/>paujla@instock.com
+                  +1 (629) 555-0129
+                  <br />
+                  paujla@instock.com
                 </div>
               </div>
             </div>
             <div className="table__actions">
-                <button className="table__delete-btn">
-                  <img src={deleteIcon} className="icon" />
-                </button>
-                <button className="table__edit-btn">
-                  <img src={editIcon} className="icon" />
-                </button>
+              <button className="table__delete-btn">
+                <img src={deleteIcon} className="icon" />
+              </button>
+              <button className="table__edit-btn">
+                <img src={editIcon} className="icon" />
+              </button>
             </div>
           </div>
         </div>
