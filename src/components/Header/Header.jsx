1a9d574ed554355f72a79d13c7c5b93ae7bc0600
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-13 11:18:44 -0500

    Styling header component and fixing padding/alignment issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import "./Header.scss";
 import logo from "/src/assets/logo/InStock-Logo_1x.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
     <header>
       <nav className="nav">
-        <div className="nav__logo-container"><NavLink to='/'><img src={logo} alt="websise logo" className="nav__logo" /></NavLink></div>
+        <div className="nav__logo-container"><Link to='/' className="nav__logo-link" ><img src={logo} alt="websise logo" className="nav__logo" /></Link></div>
         <ul className="nav__list">
-        <li className="nav__links-item"><NavLink to ='/inventory'>Inventory</NavLink></li>
-        <li className="nav__links-item"><NavLink to ='/'>Warehouses</NavLink></li>
+          <li className="nav__list-item"><NavLink to ='/' className="nav__list-link">Warehouses</NavLink></li>
+          <li className="nav__list-item"><NavLink to ='/inventory' className="nav__list-link">Inventory</NavLink></li>
         </ul>
       </nav>
     </header>
