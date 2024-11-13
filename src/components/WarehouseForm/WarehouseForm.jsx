81bbad5b78de81fb69677a16bca52246249a4247
Sarah Jahan <sarah-jahan@live.com>
2024-11-13 12:24:51 -0500

    create Warehouse Form component

---
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -0,0 +1,17 @@
+import "./WarehouseForm.scss";
+
+function WarehouseForm() {
+    return (
+      <div className="container">
+
+        <h2>Warehouse Details</h2>
+        <h2>Contact Details</h2>
+
+        
+        
+  
+      </div>
+    );
+  }
+  
+  export default WarehouseForm;
