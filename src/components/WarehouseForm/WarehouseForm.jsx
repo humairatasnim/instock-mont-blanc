ede8c601a4809796126c0e4bf7fb3fac717b47d5
Sarah Jahan <sarah-jahan@live.com>
2024-11-13 15:50:15 -0500

    add tablet styling

--- src/components/WarehouseForm/WarehouseForm.jsx
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -3,94 +3,96 @@ import "./WarehouseForm.scss";
 function WarehouseForm({action}) {
     return (
       <section className="container">
-        <h2>Warehouse Details</h2>
-        <fieldset className="form__field">
-          <label className="form__label" htmlFor="warehouse_name">
-            Warehouse Name
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="warehouse_name"
-            name="warehouse_name"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="address">
-            Street Address
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="address"
-            name="address"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="city">
-            City
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="city"
-            name="city"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="country">
-            Country
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="country"
-            name="country"
-            placeholder="insert warehouse details prop"
-          />
-          
-        </fieldset>
 
-        <h2>Contact Details</h2>
-        <fieldset className="form__field">
-          <label className="form__label" htmlFor="contact_name">
-            Contact Name
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="contact_name"
-            name="contact_name"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="contact_position">
-            Position
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="contact_position"
-            name="contact_position"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="contact_phone">
-            Phone Number
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="contact_phone"
-            name="contact_phone"
-            placeholder="insert warehouse details prop"
-          />
-          <label className="form__label" htmlFor="contact_email">
-            Email
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="contact_email"
-            name="contact_email"
-            placeholder="insert warehouse details prop"
-          />
-        </fieldset>
+        <div className="form-container">
+            <fieldset className="form__field">
+            <h2 className="subheader">Warehouse Details</h2>
+            <label className="form__label" htmlFor="warehouse_name">
+                Warehouse Name
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="warehouse_name"
+                name="warehouse_name"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="address">
+                Street Address
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="address"
+                name="address"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="city">
+                City
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="city"
+                name="city"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="country">
+                Country
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="country"
+                name="country"
+                placeholder="insert warehouse details prop"
+            />
+            </fieldset>
+
+            <fieldset className="form__field">
+            <h2 className="subheader">Contact Details</h2>
+            <label className="form__label" htmlFor="contact_name">
+                Contact Name
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="contact_name"
+                name="contact_name"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="contact_position">
+                Position
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="contact_position"
+                name="contact_position"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="contact_phone">
+                Phone Number
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="contact_phone"
+                name="contact_phone"
+                placeholder="insert warehouse details prop"
+            />
+            <label className="form__label" htmlFor="contact_email">
+                Email
+            </label>
+            <input
+                className="form__input"
+                type="text"
+                id="contact_email"
+                name="contact_email"
+                placeholder="insert warehouse details prop"
+            />
+            </fieldset>
+        </div>
 
         <div className="submit-container">
             <button className="button button-secondary">Cancel</button>
