1e75e275afb0007c79f742df647fd4c9f5a6e648
biubiubiupia <contact@tinafeng.com>
2024-11-14 14:18:32 -0500

    add function to get inventory items for given warehouse

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [warehouses, setWarehouses] = useState(null);
+  // const [inventories, setInventorie] = useState([]);
+
 
   const getWarehouses = async () => {
     try {
@@ -28,12 +30,27 @@ function App() {
     }
   };
 
+  const getInventories = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/api/inventories`);
+      setInventories(data);
+    } catch (error) {
+      console.error("Error fetching inventories:", error);
+    }
+  };
+
   useEffect(() => {
     getWarehouses();
   }, []);
 
   if (!warehouses) return <div>Loading warehouses...</div>;
-  
+
+  // useEffect(() => {
+  //   getInventories();
+  // }, []);
+
+  // if (!inventories) return <div>Loading inventories...</div>;
+
   return (
     <BrowserRouter>
       <Header />
