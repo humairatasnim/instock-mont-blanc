9a00ed900209e2f5489642666fcd1371a94d5320
Sarah Jahan <sarah-jahan@live.com>
2024-11-15 00:08:44 -0500

    create add warehouse page and component

--- src/pages/AddWarehouse/AddWarehouse.jsx
+++ src/pages/AddWarehouse/AddWarehouse.jsx
@@ -1,10 +1,22 @@
+import WarehouseForm from "../../components/WarehouseForm/WarehouseForm";
 import "./AddWarehouse.scss";
+import backbutton from "../../assets/icons/arrow_back-24px.svg"
+import { Link } from "react-router-dom"
 
-function AddWarehouse() {
+function AddWarehouse({warehouses}) {
   return (
-    <>
-      <h1>Add Warehouse</h1>
-    </>
+    <div className="container">
+    <section className="warehouse">
+    <section className="formcontainer__title">
+      <Link to="/warehouses">
+        <img className="icon" src={backbutton}/>
+      </Link>
+      <h1 className="page-header">Add Warehouse</h1>
+    </section>
+    
+    <WarehouseForm warehouses={warehouses} action={"+ Add Warehouse"}/>
+    </section>
+  </div>
   );
 }
 
