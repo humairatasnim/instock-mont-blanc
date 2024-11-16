516f26908c39daba92652a83ae39c03041cf17c3
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-16 18:34:31 -0500

    add search functionality to warehouses page

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -8,76 +8,105 @@ import Modal from "../../components/Modal/Modal";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-function Warehouses({warehouses: initialWarehouses}) {
+function Warehouses({ warehouses: initialWarehouses }) {
   const [sortOrder, setSortOrder] = useState("asc");
   const [warehouses, setWarehouses] = useState(initialWarehouses);
   const [warehouseToDelete, setWarehouseToDelete] = useState(null);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [filteredWarehouses, setFilteredWarehouses] = useState([]);
 
   function deleteWarehouseHandler(warehouse) {
     setWarehouseToDelete(warehouse);
   }
-      
-      const getWarehouses = async () => {
-        try {
-          const { data } = await axios.get(`${BASE_URL}/api/warehouses`);
-          setWarehouses(data);
-        } catch (error) {
-          console.error("Error fetching warehouses:", error);
-        }
-      };
-    
-      useEffect(() => {
-        getWarehouses();
-      }, [warehouseToDelete]);
 
-       //Sort Button Function
-
-  const handleSort = async (sortBy = "warehouse_name" || "address" || "contact_name" || "contact_email") =>  {
+  const getWarehouses = async () => {
     try {
-        const { data } = await axios.get(`${BASE_URL}/api/warehouses/`, {
-          params: { sortBy, order: sortOrder },
-        });
-          setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
-          setWarehouses(data);
-      } catch (error) {
-          console.error("Error getting warehouse data from API call", error);
-          }
-      }
+      const { data } = await axios.get(`${BASE_URL}/api/warehouses`);
+      setWarehouses(data);
+      setFilteredWarehouses(data);
+    } catch (error) {
+      console.error("Error fetching warehouses:", error);
+    }
+  };
 
-      useEffect(() => {
-        setWarehouses(warehouses);
-      }, [warehouses]);
-      
+  useEffect(() => {
+    getWarehouses();
+  }, [warehouseToDelete]);
+
+  useEffect(() => {
+    const lowerCaseKey = searchKeyword.toLowerCase();
+
+    const searchFields = [
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_phone",
+      "contact_email",
+    ];
+
+    setFilteredWarehouses(
+      warehouses.filter((warehouse) =>
+        searchFields.some((field) =>
+          warehouse[field]?.toString().toLowerCase().includes(lowerCaseKey)
+        )
+      )
+    );
+  }, [searchKeyword, warehouses]);
+
+  //Sort Button Function]
+  const handleSort = async (
+    sortBy = "warehouse_name" || "address" || "contact_name" || "contact_email"
+  ) => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/api/warehouses/`, {
+        params: { sortBy, order: sortOrder },
+      });
+      setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+      setWarehouses(data);
+    } catch (error) {
+      console.error("Error getting warehouse data from API call", error);
+    }
+  };
+
+  useEffect(() => {
+    setWarehouses(warehouses);
+  }, [warehouses]);
+
+
   if (!warehouses) return <div>Loading warehouses...</div>;
 
   return (
     <main className="container">
       <Modal
         warehouse={warehouseToDelete}
-        type={'warehouse'}
+        type={"warehouse"}
         onClose={() => setWarehouseToDelete(null)}
       />
       <section className="panel warehouses">
         <div className="warehouses__header">
           <h1 className="warehouses__title">Warehouses</h1>
           <input
-            className="warehouses__search"
+            className="inventory__search"
             type="text"
             id="name"
             name="name"
             placeholder="Search..."
+            onChange={(e) => setSearchKeyword(e.target.value)}
           />
           <Link to="warehouses/add" className="button button-primary">
             + Add New Warehouse
           </Link>
         </div>
-
         <div className="warehouses__body">
           <div className="table__header">
             <div className="table__column table__column--warehouse">
               <div className="table__header-cell table__cell--name">
                 <h2 className="table__header-text">Warehouse</h2>
-                <button className="table__sort-btn" onClick={() => handleSort("warehouse_name")}>
+                <button
+                  className="table__sort-btn"
+                  onClick={() => handleSort("warehouse_name")}
+                >
                   <img
                     src={sortIcon}
                     alt="Sort icon"
@@ -88,7 +117,10 @@ function Warehouses({warehouses: initialWarehouses}) {
 
               <div className="table__header-cell table__cell--address">
                 <h2 className="table__header-text">Address</h2>
-                <button className="table__sort-btn" onClick={() => handleSort("address")}>
+                <button
+                  className="table__sort-btn"
+                  onClick={() => handleSort("address")}
+                >
                   <img
                     src={sortIcon}
                     alt="Sort icon"
@@ -101,7 +133,10 @@ function Warehouses({warehouses: initialWarehouses}) {
             <div className="table__column table__column--contact">
               <div className="table__header-cell table__cell--contact">
                 <h2 className="table__header-text">Contact Name</h2>
-                <button className="table__sort-btn" onClick={() => handleSort("contact_name")}>
+                <button
+                  className="table__sort-btn"
+                  onClick={() => handleSort("contact_name")}
+                >
                   <img
                     src={sortIcon}
                     alt="Sort icon"
@@ -112,7 +147,10 @@ function Warehouses({warehouses: initialWarehouses}) {
 
               <div className="table__header-cell table__cell--contact-info">
                 <h2 className="table__header-text">Contact Information</h2>
-                <button className="table__sort-btn" onClick={() => handleSort("contact_email")}>
+                <button
+                  className="table__sort-btn"
+                  onClick={() => handleSort("contact_email")}
+                >
                   <img
                     src={sortIcon}
                     alt="Sort icon"
@@ -128,7 +166,7 @@ function Warehouses({warehouses: initialWarehouses}) {
           </div>
 
           <ul className="table__body">
-            {warehouses.map((warehouse) => (
+            {filteredWarehouses.map((warehouse) => (
               <WarehouseList
                 key={warehouse.id}
                 warehouse={warehouse}
